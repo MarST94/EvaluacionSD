57fc75623270f9d596f97fbd31e68679b58d878c
MarST94 <70647200+MarST94@users.noreply.github.com>
2020-09-14 02:29:25 -0600

    suma numeros

---
+++ Program2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int numero = 0;
+            const int n = 10;
+            while (numero < n)
+            {
+                numero = numero + 1;
+                suma = suma + numero;
+            }
+            System.Console.WriteLine("La suma es: " + suma);
+            Console.Read();
+        }
+    }
+}
